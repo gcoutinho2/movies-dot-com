9a9e6705699a689aa48fb57d113ac22e3b85a641
Gabriel Coutinho <gabriel.souza@resourceit.com>
2019-06-12 01:56:20 -0300

    Iniciado estrutura do projeto, adicionado pacote bootstrap e componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { LoginComponent } from './login/login.component';
+import { MoviesComponent } from './movies/movies.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
